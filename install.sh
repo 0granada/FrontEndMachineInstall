03d2f18bb0ba2894ed2379763c3d38db53a2c55c
Oscar Granada <ogranada@xumak.com>
2016-09-14 11:12:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,12 @@ brew install zsh
 
 # User Applications
 brew cask install iterm2
-brew cask install atom
-brew cask install brackets
 brew cask install slack
 brew cask install google-chrome
 brew cask install firefox
 brew cask install opera
 brew cask install skype
+brew cask install libreoffice
 
 # Ruby Version Manager
 curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
@@ -38,13 +37,59 @@ printf '\n%s\n%s\n%s\n\n' '# virtualenv' 'export WORKON_HOME=~/.virtualenvs' 'so
 printf '\n%s\n%s\n%s\n\n' '# virtualenv' 'export WORKON_HOME=~/.virtualenvs' 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.zshrc
 
 # Java install
-curl -sL http://git.io/viaTR | bash
-echo "export JAVA_HOME=\`/usr/libexec/java_home\`" >> ~/.bash_profile
-echo "export JAVA_HOME=\`/usr/libexec/java_home\`" >> ~/.zshrc
+installjava(){
+  curl -sL http://git.io/viaTR | bash
+  echo "export JAVA_HOME=\`/usr/libexec/java_home\`" >> ~/.bash_profile
+  echo "export JAVA_HOME=\`/usr/libexec/java_home\`" >> ~/.zshrc
+}
 
-# Text Editor
-# Visual Studio Code
-wget -c https://go.microsoft.com/fwlink/?LinkID=620882 -O /tmp/vscode.zip
-unzip -n /tmp/vscode -d $HOME/Applications
+# Text Editors
+installvscode(){
+  # Visual Studio Code
+  res="$1"
+  name="visual studio code"
+  echo $c
+  if [ "`python -c "print str('$name' in '$res'.lower()).lower()"`" == "true" ]; then
+    echo "Installing $name"
+    wget -c https://go.microsoft.com/fwlink/?LinkID=620882 -O /tmp/vscode.zip
+    unzip -n /tmp/vscode -d $HOME/Applications
+  else
+    echo "Avoid $name installation"
+  fi
+}
+
+installatom(){
+  # Atom
+  res="$1"
+  name="atom"
+  if [ "`python -c "print str('$name' in '$res'.lower()).lower()"`" == "true" ]; then
+    echo "Installing $name"
+    brew cask install atom
+  else
+    echo "Avoid $name installation"
+  fi
+}
+
+installbrackets(){
+  # Brackets
+  res="$1"
+  name="brackets"
+  if [ "`python -c "print str('$name' in '$res'.lower()).lower()"`" == "true" ]; then
+    echo "Installing $name"
+    brew cask install brackets
+  else
+    echo "Avoid $name installation"
+  fi
+}
+
+installEditors(){
+  res=`osascript -e "choose from list {\"Visual Studio Code\",\"Atom\",\"Brackets\"} with title \"Editors to install\" with prompt \"Choose one or more editors to install\" OK button name \"These editors\" cancel button name \"Don't install nothing.\" default items {\"visual studio code\"} with multiple selections allowed"`
+  installvscode "$res"
+  installatom "$res"
+  installbrackets "$res"
+}
+
+installjava
+installEditors
 
 
